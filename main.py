1a81616954d61526f483ea6e47a46df0a2ae21fd
Topaz <topaz1618@163.com>
2022-03-13 17:36:17 +0800

    add jenkinsfile

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+def test_demo():
+    print("Hello world!")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    test_demo()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
